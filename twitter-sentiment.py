d3c46e949a0597299fdcfc086d90d7a838055fb5
Blessin Varkey <blessinvarkey@gmail.com>
2021-02-02 01:58:50 +0530

    python file created

---
+++ twitter-sentiment.py
@@ -0,0 +1,30 @@
+import tweepy #python library for accessing Twitter API
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from textblob import TextBlob
+
+#import twitter_credentials
+
+import pandas as pd
+import numpy as np
+
+
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+
+AUTH = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+AUTH.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+API = tweepy.API(AUTH)
+public_tweets = API.search('Cosmos')
+
+for tweet in public_tweets:
+    print(tweet.text)
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+        if analysis.sentiment[0]>0:
+            print('The sentiment of the tweet is positive')
+        else:
+            print('The sentiment of the tweet is negative')
